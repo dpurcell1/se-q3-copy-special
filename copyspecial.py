e832240bb9c608331c2e78bc0d20619ae20fe5f2
dpurcell1 <dpsvr3@gmail.com>
2020-09-20 16:24:49 -0400

    Updated comments

--- copyspecial.py
+++ copyspecial.py
@@ -21,14 +21,20 @@ import argparse
 
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
+    # establishes list to hold special files as well as sp. files regex pattern
     special_files = []
     dir_regex = re.compile(r'.*__\w+__.*')
+    # establishes list of all files in specificed directory
     directory = os.listdir(dirname)
+    # loops through dir list for files that match regex pattern
     for file in directory:
         dir_match = re.findall(dir_regex, file)
         if dir_match:
+            # establishes absolute path variable from matches
             absolute_path = os.path.abspath(os.path.join(dirname, file))
+            # adds files to special files list
             special_files.append(absolute_path)
+    # Validation for no match cases
     if len(special_files) == 0:
         print("No special files exist in this directory.")
     return special_files
@@ -36,21 +42,27 @@ def get_special_paths(dirname):
 
 def create_directory(path):
     """Validation for directory creation"""
+    # check whether path exists
     if not os.path.exists(path):
+        # Error catching in case of dir creation failure
         try:
             os.makedirs(path)
         except Exception as e:
+            # Prints error to console, return false (dir createion failed)
             print(e)
             print(f'Creation of {path} was unsuccessful')
             return False
+    # return true (dir creation successful)
     return True
 
 
 def copy_to(path_list, dest_dir):
     """Copies file directory to given destination"""
+    # function exits if directory creation is unsuccessful
     if not create_directory(dest_dir):
         return
     else:
+        # copies each file in path list to newly created dir
         for file in path_list:
             shutil.copyfile(
                 file, os.path.join(dest_dir, os.path.basename(file)))
@@ -59,13 +71,17 @@ def copy_to(path_list, dest_dir):
 def zip_to(path_list, dest_zip):
     """Given a list of files and destination path,
     performs zip operation on all files in path list"""
+    # establishes list of args for zip command
     zip_run_cmd = ['zip', '-j', dest_zip]
+    # adds list of files to zip cmd
     zip_run_cmd.extend(path_list)
     print('Performing the following operation:')
-    # print(" ".join(zip_run_cmd))
-    print(zip_run_cmd)
+    print(" ".join(zip_run_cmd))
+    # error catching in case of zip failure
     try:
+        # subprocess call to zip program using arg list
         subprocess.call(zip_run_cmd)
+        # error handling message display
     except OSError as Error:
         print(Error)
         exit(1)
@@ -91,20 +107,29 @@ def main(args):
 
     # Your code here: Invoke (call) your functions
 
+    # exit program if namespace is empty
     if not ns:
         parser.print_usage()
         sys.exit(1)
-
+    # todir param provided
     if ns.todir:
+        # grab special files from src directory provided
         path_list = get_special_paths(ns.from_dir)
+        # establish destination directory variable
         home = os.environ.get('HOME')
         dest = os.path.join(home, ns.todir)
+        # copy to destination
         copy_to(path_list, dest)
+        # tozip param provided
     elif ns.tozip:
+        # grab special files from src directory provided
         path_list = get_special_paths(ns.from_dir)
+        # create zip of special files
         zip_to(path_list, ns.tozip)
     else:
+        # grab special files from src directory provided
         path_list = get_special_paths(ns.from_dir)
+        # print each special file to console
         for file in path_list:
             print(file)
 
